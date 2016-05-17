76b28b6e202bee39eb28b66ff515b9b32a89a29a
Leonardo Almeida Araújo <leonardoxsystems@gmail.com>
2016-05-17 14:13:25 -0300

    Service Provider

--- src/CodeCategory/Providers/CodeCategoryServiceProvider.php
+++ src/CodeCategory/Providers/CodeCategoryServiceProvider.php
@@ -5,13 +5,14 @@ namespace Leoalmar\CodeCategory\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class LeoalmarServiceProvider extends ServiceProvider
+class CodeCategoryServiceProvider extends ServiceProvider
 {
 
 
     public function boot()
     {
         $this->publishes([__DIR__ . '/../../resources/migrations'=> base_path('database/migrations')], 'migrations');
+        require __DIR__ . '/../routes.php';
     }
 
     public function register()
---
+++ src/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::group(['prefix'=>'categories'],function(){
+
+    Route::get('test',function(){
+        return 'test';
+    });
+
+});
